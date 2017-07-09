831f1452223d2671bd4ecb956474a968d3b46e52
Crispin Bennett <myself@crisbennett.com>
2017-07-09 17:18:39 +1000

    Rotate view as it is dragged (L24)

--- src/Deck.js
+++ src/Deck.js
@@ -11,18 +11,21 @@ class Deck extends Component {
     super(props);
     const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gesture) => {
-        // We're setting the absolute x/y to the gesture's
-        // x/y diffs.
-        // I presume this works because not setting a default in
-        // new Animated.ValueXY() implicitly defines the starting
-        // position as 0,0 ?
-        position.setValue({ x: gesture.dx, y: gesture.dy })
-      },
-      onPanResponderRelease: () => {
-      }
-    });
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove:
+          (event, gesture) => {
+            // We're setting the absolute x/y to the gesture's
+            // x/y diffs.
+            // I presume this works because not setting a default in
+            // new Animated.ValueXY() implicitly defines the starting
+            // position as 0,0 ?
+            position.setValue({ x: gesture.dx, y: gesture.dy })
+          }
+        ,
+        onPanResponderRelease: () => {
+        }
+      })
+    ;
 
     // Grider puts the panResponder in state to follow convention
     // but this is more parsimonious
@@ -35,24 +38,48 @@ class Deck extends Component {
 
   render() {
     return (
-      <Animated.View>
-        {this.renderCards()}
-      </Animated.View>
-    );
+      < Animated.View>
+        {this.renderCards()
+        }
+      </
+        Animated.View>
+    )
+      ;
+  }
+
+  getCardStyle() {
+    const position = this.position;
+    const rotate = position.x.interpolate({
+      inputRange: [ -500, 0, 500 ],
+      outputRange: [ '-120deg', '0deg', '120deg' ]
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [ { rotate } ]
+    };
   }
 
   renderCards() {
     return this.props.data.map((item, index) => {
       // temporarily limiting animation to the first visible card
-      if (index === 0) {
+      if (index === 0
+      ) {
         return (
-          <Animated.View
-            key={item.id}
-            {...this.panResponder.panHandlers}
-            style={this.position.getLayout()}
+          < Animated.View
+            key={item.id
+            }
+            {...
+              this.panResponder.panHandlers
+            }
+            style={this.getCardStyle()
+            }
           >
-            {this.props.renderCard(item)}
-          </Animated.View>
+            {
+              this.props.renderCard(item)
+            }
+          </
+            Animated.View>
         )
       }
       return this.props.renderCard(item);
